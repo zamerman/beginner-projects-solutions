1a6dc4842df43e49e41aa27346f94c9b79bd521f
Zachary Amerman <leetong.amerman@gmail.com>
2019-05-28 16:22:57 -0400

    Added loop and triple checker logic

--- pythonProjects/triples/triples.py
+++ pythonProjects/triples/triples.py
@@ -5,15 +5,36 @@ text_path = "./json/text.json"
 text_file = open(text_path, "r")
 text_dictionary = json.load(text_file)
 
-print(text_dictionary['name'])
+play = True
+while play:
+    print(text_dictionary['name'])
 
-sides = []
-sides.append(int(input(text_dictionary['prompt'].format('x'))))
-sides.append(int(input(text_dictionary['prompt'].format('y'))))
-sides.append(int(input(text_dictionary['prompt'].format('z'))))
+    sides = []
+    sides.append(int(input(text_dictionary['prompt'].format('x'))))
+    sides.append(int(input(text_dictionary['prompt'].format('y'))))
+    sides.append(int(input(text_dictionary['prompt'].format('z'))))
 
-squared_sides = []
-for side in sides:
-    squared_sides.append(side**2)
+    sq_sides = []
+    for side in sides:
+        sq_sides.append(side**2)
 
-print(squared_sides)
+    triple = False
+    if sq_sides[0] + sq_sides[1] == sq_sides[2]:
+        triple = True
+    elif sq_sides[0] + sq_sides[2] == sq_sides[1]:
+        triple = True
+    elif sq_sides[0] == sq_sides[1] + sq_sides[2]:
+        triple = True
+
+    if triple:
+        print(text_dictionary['true'])
+    else:
+        print(text_dictionary['false'])
+
+    redo = ""
+    while (redo != 'r') and (redo != 'q'):
+        print(text_dictionary['try again'])
+        redo = input()
+
+    if redo == 'q':
+        play = False
