4e1425d4f166aad9b1ce2ad13c0d83d4a84aa2af
Zachary Amerman <leetong.amerman@gmail.com>
2019-05-29 15:35:08 -0400

    Corrected code logic

--- pythonProjects/99bottles/99bottles.py
+++ pythonProjects/99bottles/99bottles.py
@@ -1,20 +1,22 @@
 #!/usr/env/bash python
 
-verse = "{} of beer on the wall, {} of beer.\nTake one down, pass it around, {} of beer on the wall."
-end_verse = "No more bottles of beer on the wall, no more bottles of beer.\nGo to the stare and by some more, {} of beer on the wall."
+verse = "\n{} of beer on the wall, {} of beer.\nTake one down, pass it around, {} of beer on the wall."
+end_verse = "\nNo more bottles of beer on the wall, no more bottles of beer.\nGo to the store and by some more, {} of beer on the wall."
 
 def bottles(n):
     if n == 1:
         return "{} bottle".format(n)
+    elif n == 0:
+        return "No more bottles"
     else:
         return "{} bottles".format(n)
 
 def lyrics(n):
     if n == 0:
-        print(end_verse.format(99))
+        print(end_verse.format(bottles(99)))
     else:
-        print(verse.format(bottles(n), bottles(n), bottles(n - 1))
+        print(verse.format(bottles(n), bottles(n), bottles(n - 1)))
         lyrics(n - 1)
 
 if __name__ == "__main__":
-    lyrics()
+    lyrics(99)
