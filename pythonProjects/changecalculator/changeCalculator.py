51c2649f6a341b0c9e1a125d4037b1eb043dfad0
Zachary Amerman <leetong.amerman@gmail.com>
2019-06-06 12:56:58 -0400

    Changed input method from sys.argv to input request

--- pythonProjects/changecalculator/changeCalculator.py
+++ pythonProjects/changecalculator/changeCalculator.py
@@ -4,33 +4,8 @@ import sys
 returnForm = "\nChange: {}\n\nQuarters: {}\nDimes: {}\nNickels: {}\nPennies: {}"
 
 
-def checkArgs(func):
-    def inside():
-        if len(sys.argv) != 2:
-            print("Must insert argument")
-            return
-        return func()
-    return inside
-
-
-def checkChange(func):
-    def inside():
-        try:
-            float(sys.argv[1])
-        except ValueError:
-            print("Argument must be float castable")
-            return
-        if sys.argv[1].find('.') + 3 != len(sys.argv[1]):
-            print("There must be two numbers after the decimal point")
-            return
-        return func()
-    return inside
-
-
-@checkArgs
-@checkChange
-def getArgs():
-    return float(sys.argv[1])
+def getInput():
+    return input("Change/Payment,Price:")
 
 
 def calculateChange(change):
@@ -62,7 +37,7 @@ def calculateChange(change):
 
 def main():
     print("\nChange Calculator")
-    args = getArgs()
+    inputString = str(getInput())
     change = calculateChange(args)
     print(returnForm.format(args,
                             change.getQ(),
